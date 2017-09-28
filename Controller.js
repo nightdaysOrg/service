f4d3462affbf158c7de770a8191ed64882f93704
niedongsheng <niedongsheng@doctorwork.com>
2017-09-28 11:11:00 +0800

    测试接口

---
+++ Controller.js
@@ -0,0 +1,20 @@
+
+let controllers = {
+
+    test : function(req,res){
+        res.send("阿斯蒂芬");
+    }
+
+
+
+
+
+
+
+}
+
+
+module.exports = {
+    root: 'wechat',
+    controllers: controllers
+}
