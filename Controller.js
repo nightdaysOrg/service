4f7127510867b9cba798820b7adee5c51903091f
niedongsheng <niedongsheng@doctorwork.com>
2017-09-28 11:26:10 +0800

    新接口

--- Controller.js
+++ Controller.js
@@ -3,7 +3,19 @@ let controllers = {
 
     test : function(req,res){
         res.send("阿斯蒂芬");
+    },
+    //保存记录
+    saveData : function(req,res){
+
+    },
+    //获取记录
+    getData: function(req,res){
+        res.send({
+            name: 123,
+            age : 234
+        });
     }
+
 
 
 
