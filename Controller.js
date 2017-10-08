53d735adb7659064da8098e26f6cf4c90c59bc80
hzCat <haizhoucat@qq.com>
2017-10-08 18:24:21 +0800

    mysql

--- Controller.js
+++ Controller.js
@@ -1,19 +1,44 @@
-
 let controllers = {
 
-    test : function(req,res){
+    test: function (req, res) {
         res.send("阿斯蒂芬");
     },
     //保存记录
-    saveData : function(req,res){
+    saveData: function (req, res) {
         console.log(req.body);
     },
     //获取记录
-    getData: function(req,res){
+    getData: function (req, res) {
         res.send({
             name: 123,
-            age : 234
+            age: 234
         });
+    },
+    login(req, res) {
+        req.on("data", (data) => {
+            var str = data.toString();
+            var obj = qs.parse(str);
+            var uname = obj.uname;
+            var upwd = obj.upwd;
+
+            var sql = "select * from timetimeuser where uname=password(?) and upwd=?";
+
+            pool.getConnection((err, conn) => {
+                if (err) {
+                    console.log(err + " GetConnect")
+                } else {
+                    console.log("sueecss");
+                    conn.query(sql, [uname, upwd], (err, result) => {
+                        if (err) {
+                            console.log(err + " query")
+                        } else {
+                            console.log(result)
+                        }
+                        conn.release()
+                    })
+                }
+            })
+        })
     }
 
 
